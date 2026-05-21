e5e5afa829dacdbd041ba6ad4918d5a19606a475
champ.it <champ.it.test@gmail.com>
2026-05-21 14:36:52 +0700

    fix: workaround for eslint 9 circular json bug during CI and build

--- web/next.config.ts
+++ web/next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: 'standalone',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
